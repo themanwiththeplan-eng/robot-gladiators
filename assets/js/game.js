10f3dd13ed40c8b2c27e835442b838d119ab84bb
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-04 21:54:36 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,13 +5,13 @@ var playerMoney = 10;
 
 // You can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-var fight = function(){
+var fight = function(enemyName){
     window.alert("Welcome to Robot Gladiators!");
     console.log(promptFight);
     
@@ -20,20 +20,20 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
   
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + " has died!");
+      window.alert(enemyNames + " has died!");
     } else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+      window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
   
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
   
     // check player's health
@@ -52,9 +52,27 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
         playerMoney = playerMoney - 2;
     }
   } else {
-    fight();
+    for(var i = 0; i < enemyNames.length; i++){
+      fight(enemyNames[i]);
+    }
   }
+}
+console.log(enemyNames.length);
+
+for(var i = 0; i < enemyNames.length; i++){
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index.");
+}
+for(var i = 0; i < enemyNames.length; i++){
+  fight(enemyNames[i])
 }
 
 
-fight();
+// Game States
+/* 
+"WIN" - Player robot has defeated all enemy-robots
+  *Fight all enemy-robots
+  *Defeat each enemy-robot
+"LOSE" - Player robot's health is zero or less
+*/
